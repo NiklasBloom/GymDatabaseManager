6f088409534afb07bc507f35c3c1046bfa3be5c7
niklasbloom <njb132@rutgers.edu>
2022-09-23 14:17:03 -0400

    new classes

--- src/GymDB/Member.java
+++ src/GymDB/Member.java
@@ -1,4 +1,50 @@
 package GymDB;
 
-public class Member {
+public class Member implements Comparable<Member>{
+    private String fname;
+    private String lname;
+    private Date dob;
+    private Date expire;
+    private Location location;
+
+    enum Location{
+        Bridgewater,
+        Edison,
+        Franklin,
+        Piscataway,
+        Somerville,
+    }
+
+
+    public Member(Location location) {
+        this.location = location;
+    }
+
+    public Member(String fname, Location location, String lname,
+                  Date dob, Date expire) {
+        this.location = location;
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
+        this.expire = expire;
+    }
+
+
+
+    @Override
+    public int compareTo(Member member) {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object Obj ) {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "yeah";
+    }
+
 }
+
--- src/GymDB/RunProject1.java
+++ src/GymDB/RunProject1.java
@@ -1,4 +1,6 @@
 package GymDB;
 
 public class RunProject1 {
+
+
 }
