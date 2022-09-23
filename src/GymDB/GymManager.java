d96567c6b39726ada1ab0f6b81bd2767988b1815
destr <yacunmaxim@gmail.com>
2022-09-23 14:51:07 -0400

    started GymManager

--- src/GymDB/GymManager.java
+++ src/GymDB/GymManager.java
@@ -1,4 +1,14 @@
 package GymDB;
+import java.util.Scanner;
 
 public class GymManager {
+    public void run(){
+        Scanner sc = new Scanner(System.in);
+        currentLine = sc.nextLine();
+        while(!currentLine.equals("Q")){
+            //do stuff
+
+            //regex match one or two uppercase characters \b[A-Z]\b|\b[A-Z][A-Z]\b
+        }
+    }
 }
