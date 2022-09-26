84ae3fc4f54366adda908dd40f75be41b4c3aee3
niklasbloom <njb132@rutgers.edu>
2022-09-26 16:31:51 -0400

    Updated some sort methods

--- src/GymDB/MemberDatabase.java
+++ src/GymDB/MemberDatabase.java
@@ -85,19 +85,6 @@ public class MemberDatabase {
     } //print the array contents as is
 
 
-
-    public void bubbleSort(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++)
-            for (int j = 0; j < n - i - 1; j++)
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[j]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-    }
     /*
     PC command, to display the list of members in the database ordered by the county names and then the zip codes;
     that is, if the locations are in the same county, ordered by the zip codes.
